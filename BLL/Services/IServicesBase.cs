c06f54d1da6beba854ee07bb1cc3890b80befc97
Hajar Alhajeh <hajar.ihab@gmail.com>
2023-12-29 14:09:48 +0200

    #seveses

---
+++ BLL/Services/IServicesBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+   public interface IServicesBase<T>
+    {
+        IQueryable<T> FindAll();
+        IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression);
+        void Create(T dto);
+        void Update(T dto);
+        void Delete(T dto);
+    }
+}
---
+++ BLL/Services/ServicesBase.cs
@@ -0,0 +1,44 @@
+﻿using DAL.Repositories.RepositoryFactory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class ServicesBase<T> : IServicesBase<T>
+    {
+        private readonly IRepositoryFactory _repository;
+
+        public ServicesBase(IRepositoryFactory repository)
+        {
+            _repository = repository;
+        }
+        public void Create(T dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T dto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
