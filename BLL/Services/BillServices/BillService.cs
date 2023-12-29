4c376342300b17c9bc4b3a295c6f170faabe8904
Hajar Alhajeh <hajar.ihab@gmail.com>
2023-12-30 00:42:52 +0200

    #editBills

---
+++ BLL/Services/BillServices/BillService.cs
@@ -0,0 +1,66 @@
+﻿using DAL.Repositories.PersonRepos;
+using DAL.Repositories;
+using BLL.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Repositories.RepositoryFactory;
+using System.Linq.Expressions;
+using AutoMapper;
+using DAL.Entities;
+
+namespace BLL.Services.BillServices
+{
+    public class BillService : IBillService
+    {
+        private readonly IRepositoryFactory _repository;
+        private readonly IMapper _mapper;
+
+        public BillService(IRepositoryFactory repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public void Create(BillDto dto)
+        {
+            Bill bill = _mapper.Map<Bill>(dto);
+            _repository.Bill.Create(bill);
+
+        }
+
+        public void Delete(BillDto dto)
+        {
+            Bill bill = _mapper.Map<Bill>(dto);
+            _repository.Bill.Delete(bill);
+
+        }
+
+        public IQueryable<BillDto> FindAll()
+        {
+
+            IQueryable<Bill> billsFromDB = _repository.Bill.FindAll();
+            IQueryable<BillDto> billsDto = billsFromDB.Select(bill => _mapper.Map<BillDto>(bill));
+
+            return billsDto;
+        }
+
+        public IQueryable<BillDto> FindByCondition(int id)
+        {
+
+            IQueryable<Bill> billsFromDB = _repository.Bill.FindByCondition(x => x.BillNo == id);
+            IQueryable<BillDto> billsDto = billsFromDB.Select(bill => _mapper.Map<BillDto>(bill));
+
+            return billsDto;
+        }
+
+        public void Update(BillDto dto)
+        {
+            Bill bill = _mapper.Map<Bill>(dto);
+            _repository.Bill.Update(bill);
+
+        }
+    }
+}
---
+++ BLL/Services/BillServices/IBillService.cs
@@ -0,0 +1,19 @@
+﻿using BLL.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services.BillServices
+{
+    public interface IBillService
+    {
+        IQueryable<BillDto> FindAll();
+        IQueryable<BillDto> FindByCondition(int id);
+        void Create(BillDto dto);
+        void Update(BillDto dto);
+        void Delete(BillDto dto);
+    }
+}
