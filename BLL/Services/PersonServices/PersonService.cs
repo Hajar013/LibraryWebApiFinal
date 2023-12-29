f63e0c7be165f47ed44d618089201c747df40f5e
Hajar Alhajeh <hajar.ihab@gmail.com>
2023-12-29 15:10:53 +0200

    #PersonServices

--- BLL/Services/PersonServices/PersonService.cs
+++ BLL/Services/PersonServices/PersonService.cs
@@ -6,10 +6,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DAL.Repositories.RepositoryFactory;
 
 namespace BLL.Services.PersonServices
 {
-    public interface PersonService : ServicesBase<PersonDto>, IPersonService
+    public  class PersonService : ServicesBase<PersonDto>, IPersonService
     {
+        private readonly IRepositoryFactory _repository;
+
+        public PersonService(IRepositoryFactory repository) : base(repository)
+        {
+            _repository = repository;
+        }
     }
 }
